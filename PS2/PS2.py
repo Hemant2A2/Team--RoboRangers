26a948ed19681f689eeac582b436db96f63383a7
Hemant Ayuj <hemant.ayuj@gmail.com>
2024-01-18 16:32:14 +0530

    contour detection in PS2.py

--- PS2/PS2.py
+++ PS2/PS2.py
@@ -4,6 +4,7 @@ import time as t
 import LaRoboLiga24
 import cv2
 import pybullet as p
+import numpy as np
 
 CAR_LOCATION = [0,0,1.5]
 
@@ -28,9 +29,9 @@ HUMANOIDS_LOCATION = dict({
 })
 
 VISUAL_CAM_SETTINGS = dict({
-    'cam_dist'       : 30,
+    'cam_dist'       : 20,
     'cam_yaw'        : 20,
-    'cam_pitch'      : -130,
+    'cam_pitch'      : -120,
     'cam_target_pos' : [0,4,0]
 })
 
@@ -39,7 +40,7 @@ os.chdir(os.path.dirname(os.getcwd()))
 env = gym.make('LaRoboLiga24',
     arena = "arena2",
     car_location=CAR_LOCATION,
-    ball_location=BALLS_LOCATION_BONOUS,  # toggle this to BALLS_LOCATION_BONOUS to load bonous arena
+    ball_location=BALLS_LOCATION,  # toggle this to BALLS_LOCATION_BONOUS to load bonous arena
     humanoid_location=HUMANOIDS_LOCATION,
     visual_cam_settings=VISUAL_CAM_SETTINGS
 )
@@ -47,9 +48,69 @@ env = gym.make('LaRoboLiga24',
 """
 CODE AFTER THIS
 """
+def masking(image , lower_lim , upper_lim):
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, lower_lim, upper_lim)
+    res = cv2.bitwise_and(image,image, mask= mask)
+    gray = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
+    canny = cv2.Canny(gray, 100, 200)
+    return canny
+
+def yellow_ball(image):
+    lower_lim = np.array([20,50,50])
+    upper_lim = np.array([40,255,255])
+    return masking(image , lower_lim, upper_lim)
+
+# def yellow_goal(image):
+#     lower_lim = np.array([20,50,50])
+#     upper_lim = np.array([40,255,255])
+#     return masking(image , lower_lim, upper_lim)
+
+def red_ball(image):
+    lower_lim = np.array([0,50,50])
+    upper_lim = np.array([9,255,255])
+    return masking(image , lower_lim, upper_lim)
+
+# def red_goal(image):
+#     lower_lim = np.array([0,50,50])
+#     upper_lim = np.array([9,255,255])
+#     return masking(image , lower_lim, upper_lim)
+
+def blue_ball(image):
+    lower_lim = np.array([90,50,50])
+    upper_lim = np.array([128,255,255])
+    return masking(image , lower_lim, upper_lim)
+
+# def blue_goal(image):
+#     lower_lim = np.array([90,50,50])
+#     upper_lim = np.array([128,255,255])
+#     return masking(image , lower_lim, upper_lim)
+
+def purple_ball(image):
+    lower_lim = np.array([130,50,50])
+    upper_lim = np.array([160,255,255])
+    return masking(image , lower_lim, upper_lim)
+
+# def purple_goal(image):
+#     lower_lim = np.array([130,50,50])
+#     upper_lim = np.array([160,255,255])
+#     return masking(image , lower_lim, upper_lim)
 
 while True:
     img = env.get_image(cam_height=0, dims=[512, 512])
+
+    # Search order:
+    # yellow ball , blue ball , red ball , purple ball
+    # issue - not able to detect blue goal post
+    # after holding the ball crop the image to remove the part containing ball
+
+    canny = blue_ball(img)
+    ret, thresh = cv2.threshold(canny, 127, 255, 0)
+    contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    if contours:
+        cnt = max(contours , key=cv2.contourArea)
+        cv2.drawContours(img, [cnt], 0, (0,255,0), 2)
+
     cv2.imshow("image",img)
     ## Manual control code
     keys = p.getKeyboardEvents()

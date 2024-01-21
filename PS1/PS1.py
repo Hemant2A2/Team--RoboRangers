7f1a7900b54d06ce2a301e8aa5a8ea3211d2358b
Hemant Ayuj <hemant.ayuj@gmail.com>
2024-01-22 00:33:23 +0530

    final code for PS1

--- PS1/PS1.py
+++ PS1/PS1.py
@@ -10,7 +10,8 @@ import math
 CAR_LOCATION = [-25.5,0,1.5]
 ROI_vertices = [(0,0),(0,450),(512,450),(512,0)]
 ROI_vertices= np.array([ROI_vertices],np.int32)
-
+pre = 0
+i = 0
 
 VISUAL_CAM_SETTINGS = dict({
     'cam_dist'       : 40,
@@ -30,17 +31,17 @@ env = gym.make('LaRoboLiga24',
 """
 CODE AFTER THIS
 """
+### Team--RoboRangers/urdf/arena/robo_lega/materials ---> use the track.jpg image in drive here
 
 def ROI(img):
     mask = np.zeros_like(img)
     match_mask_color = 255
     cv2.fillPoly(mask, ROI_vertices, match_mask_color)
     masked_img = cv2.bitwise_and(img, mask)
-    #blurred = cv2.GaussianBlur(masked_img, (5, 5), 0)
-    #edges = cv2.Canny(blurred, 50, 150)
-    _, thresh = cv2.threshold(masked_img, 127, 255, 0)
+    ret, thresh = cv2.threshold(masked_img, 127, 255, 0)
     contours,_ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     return contours
+
 
 def detect_center(image):
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
@@ -53,84 +54,69 @@ def detect_center(image):
     edges = cv2.Canny(blurred, 50, 150)
     return ROI(edges)
 
-def control(mode):
-    vel = 3
-    if (mode == "S"):
-        vel = 8
+
+def control(mode, speed):    
+    vel = abs(5 + speed)
+    if (vel <= 1.25):
+        vel = 2.6    
+    print(speed)
+    if (mode == "S"):  
+        vel = 11
         env.move([[vel, vel], [vel, vel]])
     elif (mode == "R"):
-        env.move([[0,0],[0,0]])
-        env.move([[-vel, vel], [-vel, vel]])
+        env.move([[(-vel), vel], [(-vel), vel]])  
+        
     elif (mode == "L"):
-        env.move([[0,0],[0,0]])
-        env.move([[vel, -vel], [vel, -vel]])
+        env.move([[vel, (-vel)], [vel, (-vel)]])
+       
 
-def move(contours):
+def move(contours, preError, integral):
     if contours:
         cnt = max(contours, key= cv2.contourArea)
-
-        _,cols = img.shape[:2]
+        rows,cols = img.shape[:2]
         [vx,vy,x,y] = cv2.fitLine(cnt, cv2.DIST_L2,0,0.01,0.01)
-        left_y = int((-x*vy/vx) + y)
-        right_y = int(((cols-x)*vy/vx)+y)
+        lefty = int((-x*vy/vx) + y)
+        righty = int(((cols-x)*vy/vx)+y)
         m = vy/vx
-
-        cv2.line(img,(int(cols-1),right_y),(0,left_y),(0,255,0),2)
-        cv2.line(img, (256, 512), (256, 0), (0,0,255), 2)
-
-        offset = x - 256
-        print(m, "\t OFFSET: ", offset)
+        new_y = 156
+        new_x = (new_y - (y-m*x))/m
+        offset = int((new_x - 256)/10)
+        speed = pid(x, preError, integral)
 
         #Straight
-        if  (-25 < offset < 25):
-            control("S")
+        if  (-5 < offset < 5):
+            speed = pid(x, preError, integral)    
+            control("S",speed)
+            
         #Right
-        elif offset < -25:
-            control("R")
+        elif offset < -5:
+            speed = pid(x, preError, integral)    
+            control("R", speed)
         #Left
-        elif offset > 25:
-            control("L")
+        elif offset > 5:
+            speed = pid(x, preError, integral)    
+            control("L", speed)
+            
 
-# def pid_controller(error, preError , integral):
-#     Kp = 0.1
-#     Ki = 0.01
-#     Kd = 0.05
+def pid(value, preError, integral):
+    Kp = 0.0025
+    Ki = 0.04
+    Kd = 0.05
+    error = (value - 256)
+    preError += error 
+    proportional = Kp * error
+    integral += Ki * preError
+    derivative = Kd * (error - preError)
 
-#     proportional = Kp * error
-#     integral += Ki * error
-#     derivative = Kd * (error - preError)
-
-#     return proportional + integral + derivative
+    return proportional + integral + derivative
 
 
+    
 while True:
     img = env.get_image(cam_height=0, dims=[512, 512])
     contours = detect_center(img)
-    
-    move(contours)
-    cv2.imshow("image", img)
 
-    ## Manual control code ---------------------------------------------------------------------------------------------------------------------------------------
-    keys = p.getKeyboardEvents()
-    rot = 3
-    speed = 5
-    #vel = [[0,0],[0,0]]
-
-    if p.B3G_LEFT_ARROW in keys and keys[p.B3G_LEFT_ARROW] & p.KEY_WAS_TRIGGERED:
-        env.move([[-rot, rot], [-rot, rot]])
-
-    elif p.B3G_RIGHT_ARROW in keys and keys[p.B3G_RIGHT_ARROW] & p.KEY_WAS_TRIGGERED:
-        env.move([[rot, -rot], [rot, -rot]])
-
-    elif p.B3G_UP_ARROW in keys and keys[p.B3G_UP_ARROW] & p.KEY_WAS_TRIGGERED:
-        env.move([[speed,speed ], [speed, speed]])
-
-    elif p.B3G_DOWN_ARROW in keys and keys[p.B3G_DOWN_ARROW] & p.KEY_WAS_TRIGGERED:
-        env.move([[-speed, -speed], [-speed, -speed]])
-
-    elif p.B3G_SPACE in keys and keys[p.B3G_SPACE] & p.KEY_WAS_TRIGGERED:
-        env.move([[0, 0], [0, 0]])
-        
+    move(contours, pre, i)
 
     k = cv2.waitKey(1)
     if k == ord('q'):
